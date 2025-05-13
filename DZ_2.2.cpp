8a39001491eb044cba187f7b122b6e32be862149
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-05-13 03:50:26 +0300

    Add files via upload

---
+++ DZ_2.2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+int main() {
+	std::string word;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout << "Введите слово:" << std::endl;
+	std::cin >> word;
+	std::cout << "Вы ввели:\n" << word << std::endl;
+
+	return 0;
+}
