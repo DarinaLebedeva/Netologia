cf17937f82e765fdf1ffb880862f1e716f3769f8
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-05-13 03:38:07 +0300

    Add files via upload

---
+++ Netologia.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() {
+	int number; 
+
+	SetConsoleOutputCP(1251);
+	std::cout << "Введите число:" << std::endl;
+	std::cin >> number;
+	std::cout << "Вы ввели:\n" << number << std::endl;
+
+	return 0;
+}
