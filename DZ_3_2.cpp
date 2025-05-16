00fdc0694c816a1331abb72f662f0003ccda865e
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-05-17 01:24:50 +0300

    Add files via upload

---
+++ DZ_3_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double num1;
+    std::cout << "Введите первое число:";
+    std::cin >> num1;
+
+    double num2;
+    std::cout << "Введите второе число:";
+    std::cin >> num2;
+
+    double num3;
+    std::cout << "Введите третье число:";
+    std::cin >> num3;
+
+    double max = (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ? num2 : num3);
+    double mid = (num1 < num2) ? ((num1 > num3) ? num1 : ((num2 < num3) ? num2 : num3)) : ((num1 < num3) ? num1 : ((num2 > num3) ? num2 : num3));
+    double min = (num1 < num2) ? ((num1 < num3) ? num1 : num3) : ((num2 < num3) ? num2 : num3);
+
+    std::cout << "Результат: " << max << " " << mid << " " << min << std::endl;
+
+
+    return 0;
+}
