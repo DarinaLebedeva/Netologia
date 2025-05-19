e08cc51671ab4309b44b6e342bc57cea9907f64b
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-05-20 01:55:32 +0300

    Add files via upload

---
+++ DZ_4_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int num;
+    int sum=0;
+    
+    do {
+        std::cout << "Введите целое число или число '0', чтобы закончить: " << std::endl;
+        std::cin >> num;
+
+        sum += static_cast <int> (num);
+    } while (num != 0);
+
+    std::cout << sum;
+    return 0;
+}
