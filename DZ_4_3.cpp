58ee1f4c8a923c7525f0a6702c46b4b45945ea35
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-05-20 02:09:35 +0300

    Add files via upload

---
+++ DZ_4_3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите целое число:\n";
+
+    int num;
+    std::cin >> num;
+    
+    for (int i = 1; i <= 10; i++) {
+        int res = num*i;
+        std::cout << num << " x " << i << " = " << res << std::endl;
+    }
+}
