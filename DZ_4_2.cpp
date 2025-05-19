3207e858c2bbdc1999e3bde5c6352a2dda2eb097
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-05-20 02:03:11 +0300

    Add files via upload

---
+++ DZ_4_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите целое число:\n";
+
+    int num;
+    std::cin >> num;
+
+    int sum=0;
+
+    while (num > 0){
+        int num_ost = num % 10;
+        sum += num_ost;
+        num = num / 10;
+    }
+    std::cout << "Сумма цифр: " << sum;
+}
