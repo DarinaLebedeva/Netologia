5320c1d1fdeb027badc1c86494f43dc77451b6d6
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-06-10 13:17:26 +0300

    Add files via upload

---
+++ DZ_5.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int arr[] = { 4, 2, 1, 5, 3 };
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    std::cout << "Минимум: " << min << std::endl;
+    std::cout << "Максимум: " << max << std::endl;
+}
