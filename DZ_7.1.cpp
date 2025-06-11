65783e3e419de95d62d46835836d995ab490d0a3
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-06-11 14:34:09 +0300

    Add files via upload

---
+++ DZ_7.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    int constI;
+    short constS;
+    long constL;
+    long long constLL;
+    float constF;
+    double constD;
+    long double constLD;
+    bool constB;
+
+    std::cout << "int: " << &constI << " " << sizeof(constI) << std::endl;
+    std::cout << "short: " << &constS << " " << sizeof(constS) << std::endl;
+    std::cout << "long: " << &constL << " " << sizeof(constL) << std::endl;
+    std::cout << "long long: " << &constLL << " " << sizeof(constLL) << std::endl;
+    std::cout << "float: " << &constF << " " << sizeof(constF) << std::endl;
+    std::cout << "double: " << &constD << " " << sizeof(constD) << std::endl;
+    std::cout << "long double: " << &constLD << " " << sizeof(constLD) << std::endl;
+    std::cout << "bool: " << &constB << " " << sizeof(constB) << std::endl;
+}
