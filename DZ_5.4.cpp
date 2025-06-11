58cd45df04bcd152459860ab628bc84487868b67
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-06-11 10:42:13 +0300

    Add files via upload

---
+++ DZ_5.4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int arr[10] = { 10, 3, 4, 1, 8, 9, 7, 2, 6, 5 };
+
+    std::cout << "Массив до сортировки: ";
+    for (int i = 0; i < 10; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int j = 0; j < 10 - 1; j++) {
+        for (int i = 9; i > j; i--) {
+            if (arr[i] < arr[i - 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+            }
+        }
+    }
+
+    std::cout << "Массив после сортировки: ";
+    for (int i = 0; i < 10; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
