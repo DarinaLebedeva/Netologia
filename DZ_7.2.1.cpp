d1474576e0b1a30feecf16eef1324db4d1a8717e
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-06-11 21:59:07 +0300

    Add files via upload

---
+++ DZ_7.2.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void swap(int& a, int& b) {
+	int temp = b;
+	b = a;
+	a = temp;
+}
+
+
+int main(int argc, char** argv)
+{
+	int a = 5, b = 8;
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swap(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	return 0;
+}
