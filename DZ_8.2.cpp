08dc4b9442a139cb6425e8854eb3744f88410f08
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-06-11 17:34:22 +0300

    Add files via upload

---
+++ DZ_8.2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+namespace addition {
+    int calc(int x, int y) {
+        return x + y;
+    }
+}
+
+namespace substraction {
+    int calc(int x, int y) {
+        return x - y;
+    }
+}
+
+namespace multiplication {
+    int calc(int x, int y) {
+        return x * y;
+    }
+}
+
+namespace division {
+    int calc(int x, int y) {
+        return x / y;
+    }
+}
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int x = 6;
+    int y = 9;
+
+    std::cout << "x = " << x << ", y = " << y << std::endl;
+
+    std::cout << "Сложение: " << addition::calc(x, y) << std::endl;
+    std::cout << "Вычитание: " << substraction::calc(x, y) << std::endl;
+    std::cout << "Умножение: " << multiplication::calc(x, y) << std::endl;
+    std::cout << "Деление: " << division::calc(x, y) << std::endl;
+}
