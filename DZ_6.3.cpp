926228f99d368926cd22cb8d316c03e8fe907280
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-06-11 12:32:41 +0300

    Add files via upload

---
+++ DZ_6.3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int fibonacci(int num) {
+    if (num == 0) {
+        return 0; 
+    }
+    else if (num == 1) {
+        return 1; 
+    }
+    else {
+        return fibonacci(num - 1) + fibonacci(num - 2); 
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите число: ";
+
+    int count;
+    std::cin >> count;
+
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 0; i < count; i++) {
+        std::cout << fibonacci(i) << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
