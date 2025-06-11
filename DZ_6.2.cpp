3859a7b3dbb884348355af3d7921c2dfa1d27d86
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-06-11 12:11:03 +0300

    Add files via upload

---
+++ DZ_6.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void toPower(int value, int power) {
+    int result = 1;
+    for (int i = 0; i < power; i++) {
+        result *= value;
+    }
+
+    std::cout << value << " в степени " << power << " = " << result << std::endl;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    toPower(5, 2);
+    toPower(3, 3);
+    toPower(4, 4);
+}
+
