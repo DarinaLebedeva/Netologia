1e5ac1fd90b2e3ed2b7d5f765415ac7b846dc235
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-06-13 00:42:57 +0300

    Add files via upload

---
+++ DZ_10.1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите размер массива: ";
+
+    int size;
+    std::cin >> size;
+
+    int* int_ptr = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        std::cout << "arr[" << i << "] = ";
+        int num;
+        std::cin >> num;
+
+        int_ptr[i] = num;
+    }
+
+    std::cout << "Введенный массив: ";
+
+    for (int i = 0; i < size; i++) {
+        std::cout << int_ptr[i] << " ";
+    }
+    
+    delete[] int_ptr;
+}
+
