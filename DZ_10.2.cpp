a712105c79a07ade758c50b6b4513a892758c707
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-06-13 01:02:02 +0300

    Add files via upload

---
+++ DZ_10.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+double * create_array(int size) {
+    double* arr = new double[size]();
+    return arr;
+}
+
+void print_array(double* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " "; 
+    }
+    std::cout << std::endl; 
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите размер массива: ";
+
+    int size;
+    std::cin >> size;
+
+    double* external_arr = create_array(size);
+
+    std::cout << "Введенный массив: ";
+    print_array(external_arr, size);
+
+    delete[] external_arr;
+
+    return 0;
+}
