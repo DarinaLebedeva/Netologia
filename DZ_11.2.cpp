bf9a2c3fe617bf9b1f97be1b9fbcaf733358b5a1
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-07-30 11:48:10 +0300

    Add files via upload

---
+++ DZ_11.2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string hiddenWord = "малина";
+    std::string userWord;
+
+    do {
+        std::cout << "Введите слово: ";
+        std::cin >> userWord;
+        if (hiddenWord != userWord) {
+            std::cout << "Неправильно!\n";
+        }
+    } while (hiddenWord != userWord);
+
+    std::string finish = "Правильно! Вы победили! Загаданное слово - " + hiddenWord + "!";
+    std::cout << finish << std::endl;
+
+    return 0;
+}
