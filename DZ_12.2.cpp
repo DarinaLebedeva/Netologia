1a218c91644f084b6360976ba5a2e81af8025f2b
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-08-01 18:52:15 +0300

    Add files via upload

---
+++ DZ_12.2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ifstream fin("C:\\Users\\мвидео\\Desktop\\in.txt");
+
+    if (!fin.is_open()) {
+        std::cout << "Ошибка открытия файла!" << std::endl;
+        return 1;
+    }
+
+    int size;
+    fin >> size; 
+
+    int* array = new int[size];
+
+    for (int i = 0; i < size; ++i) {
+        fin >> array[i];
+    }
+
+    fin.close();
+
+    for (int i = size - 1; i >= 0; --i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] array;
+
+    return 0;
+}
