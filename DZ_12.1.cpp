9ec78b1013050728356343eb008e42d63be93bba
Darina <147654216+DarinaLebedeva@users.noreply.github.com>
2025-08-01 18:51:44 +0300

    Add files via upload

---
+++ DZ_12.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+
+int main()
+{
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+
+    std::ifstream fin("C:\\Users\\мвидео\\Desktop\\in.txt");
+
+    if (!fin.is_open()) {
+        std::cout << "Ошибка открытия файла." << std::endl;
+        return 1;
+    }
+
+    std::string word;
+    while (fin >> word) { 
+        std::cout << word << std::endl; 
+    }
+
+    fin.close();
+
+    return 0;
+}
