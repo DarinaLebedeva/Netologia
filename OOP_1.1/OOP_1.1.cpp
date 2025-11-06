c5f5380158eaf5dd4231365d519ebf0346728d88
Darina Lebedeva <147654216+DarinaLebedeva@users.noreply.github.com>
2025-11-07 00:26:30 +0300

    Add files via upload

---
+++ OOP_1.1/OOP_1.1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Address {
+private:
+    std::string city;
+    std::string street;
+    int houseNumber;
+    int apartmentNumber;
+
+public:
+    Address(const std::string& city,
+            const std::string& street,
+            int houseNumber,
+            int apartmentNumber)
+        : city(city), street(street), houseNumber(houseNumber), apartmentNumber(apartmentNumber) {
+    }
+
+    std::string getOutputAddress() const {
+        return city + ", " + street + ", " + std::to_string(houseNumber) + ", " + std::to_string(apartmentNumber);
+    }
+};
+
+int main() {
+    std::ifstream inputFile("input.txt");
+    std::ofstream outputFile("output.txt");
+
+    if (!inputFile.is_open() || !outputFile.is_open()) {
+        std::cerr << "Не получилось открыть файл :(" << std::endl;
+        return 1;
+    }
+
+    int n;
+    inputFile >> n;
+    inputFile.ignore();
+
+    std::vector<Address> addresses;
+
+    for (int i = 0; i < n; ++i) {
+        std::string city, street;
+        int houseNumber, apartmentNumber;
+
+        std::getline(inputFile, city);
+        std::getline(inputFile, street);
+        inputFile >> houseNumber >> apartmentNumber;
+        inputFile.ignore();
+
+        addresses.emplace_back(city, street, houseNumber, apartmentNumber);
+    }
+        outputFile << n << std::endl;
+
+        for (int i = n - 1; i >= 0; --i) {
+            outputFile << addresses[i].getOutputAddress() << std::endl;
+        }
+
+        inputFile.close();
+        outputFile.close();
+
+        return 0;
+    
+}
