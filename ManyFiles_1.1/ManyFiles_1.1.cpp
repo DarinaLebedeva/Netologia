c37dab82daf7621f1753b013ea49a9f93618813f
Darina Lebedeva <147654216+DarinaLebedeva@users.noreply.github.com>
2025-11-10 10:39:47 +0300

    Add files via upload

---
+++ ManyFiles_1.1/ManyFiles_1.1.cpp
@@ -0,0 +1,29 @@
+﻿#include "Header.h" 
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+
+double add(double a, double b) {
+    return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    if (b == 0) {
+        throw std::runtime_error("Деление на ноль невозможно!");
+    }
+    else {
+        return a / b;
+    }
+}
+
+double power(double base, double exponent) {
+    return std::pow(base, exponent);
+}
