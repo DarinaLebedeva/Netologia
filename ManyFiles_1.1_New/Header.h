899d2d93323c125efaac2b1d46a545b24459f9fd
Darina Lebedeva <147654216+DarinaLebedeva@users.noreply.github.com>
2025-11-18 00:08:29 +0300

    Add files via upload

---
+++ ManyFiles_1.1_New/Header.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+double power(double base, double exponent);
+
+#endif 
